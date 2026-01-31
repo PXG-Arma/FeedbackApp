c5994a589c2b59991f80507a1044e194047a1c24
mfpavez <mfpaveza@gmail.com>
2026-01-31 10:02:49 -0300

    Deploy: Final bundle with Material Icons

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "141dac310f7d31fc876039b03b422299
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "7c1a2a10e4121af824ed291ffb25d51a",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "8b4e9eb9cf22337163ae0975e4265b77",
+"flutter_bootstrap.js": "c6435815f953c8984141c25bee3dbf09",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
